1c4468ad5bd29f65c81b0f51ce45a5dd612ce3bf
Kamil Trybel <ktrybel@teb-gdansk.ad>
2024-01-28 09:44:08 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Pobieramy diva i button
+
+// Dwie tablice z imionami i prefixami
+
+// Funkcja losująca imię i prefix i wyświetlająca info w divie
+
+// Nasłuchiwanie na buttonie na powyższą funkcję
+
